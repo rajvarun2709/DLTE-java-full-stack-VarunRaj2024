f53afedce09660711480708f7ad39f839475793c
rajvarun2709 <rajvarun2709@gmail.com>
2024-03-07 12:07:12 +0530

    final

--- DLTE-Java/DLTE-Java-168/src/main/java/basic/services/Sip.java
+++ DLTE-Java/DLTE-Java-168/src/main/java/basic/services/Sip.java
@@ -1,20 +1,18 @@
 package basic.services;
-
 import java.util.Scanner;
-
-public class SIP {
+public class Sip {
     public static void main(String[] args) {
         long invest;
         double interest;
         int year;
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter monthly investment");
-        invest=sc.nextLong();
+        invest=scanner.nextLong();
         System.out.println("Enter Expected Interest");
-        interest=sc.nextDouble();
+        interest=scanner.nextDouble();
         interest/=(12*100);
         System.out.println("Enter Time");
-        year=sc.nextInt();
+        year=scanner.nextInt();
         year*=12;
         System.out.println("Total Return "+(invest*((Math.pow((1+interest),year)-1)/interest)*(1+interest)));
     }
